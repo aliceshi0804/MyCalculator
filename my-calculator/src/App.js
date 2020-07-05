819087fc1a3f1e4a5d7c23a463a4f870f28419e8
Qing Shi <aliceshi0804@gmail.com>
2020-07-05 15:00:25 -0700

    Add calculator layout

--- my-calculator/src/App.js
+++ my-calculator/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './styles/styles.css';
+import Calculator from './layout/Calculator/Calculator';
 
 const app = () => (
   <div className="app">
-    
+    <Calculator />
   </div>
 );
 
